96d15ad4158baff962bd52a7ff7c73fdd6721254
rimayu0815 <rikussyuu@gmail.com>
2020-11-17 23:13:19 +0900

    3.戦車から砲弾を発射する 4.砲弾でオブジェクト(障害物)を破壊する

---
+++ Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    //接触したオブジェクトが引数otherとして渡される
+    {
+        if(other.CompareTag("Shell"))
+        //接触したオブジェクトのタグが"Shell"の時
+        {
+            Destroy(this.gameObject);
+            //このオブジェクトを破壊する
+
+            Destroy(other.gameObject);
+            //接触したオブジェクトを破壊する
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ShotShell.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotShell : MonoBehaviour
+{
+    public float shotSpeed;
+
+    [SerializeField]
+    private GameObject shellPrefab;
+    //SerializeFieldにするとprivateでもInspector上から設定できる
+
+    [SerializeField]
+    private AudioClip shotSound;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))//もしスペースキーが押されたら
+        {
+            GameObject shell = Instantiate(shellPrefab, transform.position, Quaternion.identity);
+            //shellPrefabとtransformのpositionの値とQuaternionのidentity(無回転のQuarternion)をInstantiateメソッドで受け取ってshellに代入する
+
+            Rigidbody shellRb = shell.GetComponent<Rigidbody>();
+
+            shellRb.AddForce(transform.forward * shotSpeed);
+            //shellRb変数にAddForceメソッドを使う、forward(0,0,1)
+
+            Destroy(shell, 3.0f);
+            //shellを三秒後に破壊する
+
+            AudioSource.PlayClipAtPoint(shotSound, transform.position);
+            //shotSoundを流す
+
+        }
+    }
+}
